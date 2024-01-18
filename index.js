8298e945a12fe1d5ff169e2aa3ca4388e05338e4
Edgar Damatian <damatian.e@gmail.com>
2024-01-18 15:13:38 -0800

    done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Your code here
+function moveDodgerLeft() {
+    const dodger = document.getElementById('dodger');
+    const currentLeft = parseInt(dodger.style.left, 10);
+    if (currentLeft > 0) {
+      dodger.style.left = `${currentLeft - 1}px`;
+    }
+  }
+  function moveDodgerRight() {
+    const dodger = document.getElementById('dodger');
+    const currentLeft = parseInt(dodger.style.left, 10);
+    if (currentLeft < (window.innerWidth - dodger.offsetWidth)) {
+      dodger.style.left = `${currentLeft + 1}px`;
+    }
+  }
